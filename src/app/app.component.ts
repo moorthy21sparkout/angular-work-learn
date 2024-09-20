82631ace1a81dbe01b0f8afa99f0ceecccfd9504
moorthy21sparkout <moorthy@sparkouttech.com>
2024-09-20 14:50:04 +0530

    Drag-and-Drop -in-Task-List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,36 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigateComponent } from "./navigate/navigate.component";
 import { TaskComponent } from "./task/task.component";
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigateComponent, TaskComponent],
+  imports: [RouterOutlet, NavigateComponent, TaskComponent, DragDropModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-work-learn';
+  task = [
+    { title: "html" },
+    { title: "css" },
+    { title: "bootstrap" },
+    { title: "javascript" },
+    { title: "Reactjs" },
+    { title: "Angularjs" },
+    { title: "C++" },
+    { title: "pythan" }
+  ];
+  /**
+   * 
+   * @param event 
+   */
+  drop(event: CdkDragDrop<any[]>) {
+    moveItemInArray(this.task, event.previousIndex, event.currentIndex);
+  }
+
+
 }
